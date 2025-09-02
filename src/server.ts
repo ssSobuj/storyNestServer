32238156abd8a02bc477308dc0837d225e45f889
sobujmia5858 <sabuj.mia@acotegroup.com>
2025-09-02 23:06:30 +0600

    chagen fronend url

--- src/server.ts
+++ src/server.ts
@@ -24,7 +24,11 @@ const PORT = process.env.PORT || 5000;
 
 // 1. CORS Middleware
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://story-nest-topaz.vercel.app"],
+  origin: [
+    "http://localhost:3000",
+    "https://storyy-nest.vercel.app",
+    "https://story-nest-topaz.vercel.app",
+  ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
